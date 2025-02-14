f2fd84b725bdad33cb297cfc6af54bce33f16d24
Moul Channrith <moul.channrith@passapptech.com.kh>
2025-02-14 15:24:50 +0700

    fix signupRoute

--- src/routes/signupRoute.js
+++ src/routes/signupRoute.js
@@ -5,9 +5,9 @@ import { myUserSignupAction } from '../action';
 const router = express.Router();
 
 router.post('/signup', signupValidator, runValidation, async (req, res) => {
-  const { data, error, message, status } = await myUserSignupAction(
-    ...req.body
-  );
+  const { data, error, message, status } = await myUserSignupAction({
+    ...req.body,
+  });
   return res.status(status).json({
     error,
     message,
